f24838f0eddd6d55482a8373322802ad7e595eca
Marcos Paulo da Costa <marcos_nokbuk@hotmail.com>
2022-05-17 16:58:23 -0300

    Treino JS

---
+++ Certified_Tech_Developer-Bimestre3/Front-End_II/Aula 2 - Assincrona/js/letIn.js
@@ -0,0 +1,14 @@
+let pessoa = {
+    nome: "Marcos",
+    idade: 24,
+    profissao: "Developer"
+} 
+
+let carro = {
+    nome: "fox",
+    ano: "2011"
+}
+
+for(let prop in carro){
+    console.log();
+}
