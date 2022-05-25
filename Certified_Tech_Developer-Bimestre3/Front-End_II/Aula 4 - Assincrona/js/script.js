19f9128e7d3fd4a20439e78a58386d3e00bc7a8f
Marcos Paulo da Costa <marcos_nokbuk@hotmail.com>
2022-05-25 08:19:28 -0300

    .

--- Certified_Tech_Developer-Bimestre3/Front-End_II/Aula 4 - Assincrona/js/script.js
+++
@@ -1,3 +0,0 @@
-var teste1 = document.querySelector("#t1")
-var teste2 = document.querySelectorAll("#t2")
-var teste3 = document.getElementById("t3")
---
+++ Certified_Tech_Developer-Bimestre3/Front-End_II/Aula 4 - Assincrona/script.js
@@ -0,0 +1,5 @@
+var titulo = document.querySelectorAll("article")
+
+for (let abc in titulo){
+    console.log(titulo[abc]);
+}
